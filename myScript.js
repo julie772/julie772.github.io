8bd31da6c29ffbf40134616d11a7a988468a0ac6
julie772 <julie.dyer92@yahoo.com>
2016-12-05 16:57:47 -0800

    update both files

--- myScript.js
+++ myScript.js
@@ -3,3 +3,11 @@
 	document.getElementById("demo2").innerHTML="This is my first JavaScript function.";
 
 }
+
+function toCelsius(fahreheit){
+ 	
+	return (5/9) *(fahreheit -32); 
+    
+
+}
+document.getElementId("demo1").innerHTML = The Temperature is " + toCelsius(77) + " Celsius
