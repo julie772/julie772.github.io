8bbdf31cd2cdd19b4f2d431644322afae79052f4
julie772 <julie.dyer92@yahoo.com>
2017-06-13 18:08:14 -0700

    Create while.js

--- js/while.js
+++ js/while.js
@@ -5,14 +5,15 @@ var msg = '';
 
 if (operator == 'addition'){
   while (i < 11){
-    msg += i + '+' + table + ' = ' + (i + table) + '<br/>';
+    msg += i + '+' + table + ' = ' + (i + table) + '<br />';
     i++;
    }
   else{
     while (i < 11){
-      msg += i + ' x ' + table + ' = ' + (i * table) + '<br/>';
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
       }
   }
+  
    var el = document.getElementById('blackboard');
    el.innerHTML = msg;
